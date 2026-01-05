190a4ca7a25097a40439f36775b9dc0144fd28e7
Tristan <tristanacosta03@gmail.com>
2026-01-05 22:17:39 +0800

    Create playRound function and declare more variables

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 
 function getComputerChoice(number) 
@@ -22,5 +26,47 @@ function getHumanChoice()
     return choice;
 }
 
-console.log(getComputerChoice(randomNumber));
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice)
+{
+    humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === "scissors")
+    {
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock")
+    {
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper")
+    {
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock")
+    {
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper")
+    {
+        console.log("You lose! Paper beats rock.");
+        computerScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors")
+    {
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+    }
+    else (humanChoice === computerChoice)
+    {
+        console.log("It's a tie!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
