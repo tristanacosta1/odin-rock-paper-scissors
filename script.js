97b1c889179bbe00560e38f0e156a62739a37b2d
Tristan <tristanacosta03@gmail.com>
2026-01-05 16:41:43 +0800

    Add and finish getComputerChoice function  and add getHumanChoice function

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+function getComputerChoice(number) 
+{
+    if (number === 1) 
+    {
+        return "rock";
+    }
+    else if (number === 2) 
+    {
+        return "paper";
+    }
+    else 
+    {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() 
+{
+    prompt("Rock, paper, or scissors?");
+}
+
+console.log(getComputerChoice(randomNumber));
